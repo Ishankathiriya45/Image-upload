1f45f990ceb101da50318969f11733d4df314212
Ishankathiriya <pishan486@outlook.com>
2025-04-09 16:50:05 +0530

    one changes

--- multerImgUpload.js
+++ multerImgUpload.js
@@ -1,5 +1,5 @@
 const multer = require('multer')
-console.log('Image upload project')
+
 const storageImg = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/multer')
